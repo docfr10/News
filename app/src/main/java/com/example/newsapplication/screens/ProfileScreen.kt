da55c66adb030ffac50d3bc22395ad58b0e16d91
docfr10 <docfr10@yandex.ru>
2022-10-23 13:36:07 +0300

    Fixed log out

--- app/src/main/java/com/example/newsapplication/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/newsapplication/screens/ProfileScreen.kt
@@ -1,15 +1,18 @@
 package com.example.newsapplication.screens
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -21,8 +24,9 @@ import com.google.firebase.auth.FirebaseAuth
 @Composable
 fun ProfileScreen(auth: FirebaseAuth) {
     val cUser = auth.currentUser
-
     val context = LocalContext.current
+
+    val touchCounter = remember { mutableStateOf(0) }
 
     // Column Composable
     Column(
@@ -37,14 +41,22 @@ fun ProfileScreen(auth: FirebaseAuth) {
         Icon(
             imageVector = Icons.Default.Person,
             contentDescription = "profile",
-            tint = Purple500
+            tint = Purple500,
+            modifier = Modifier.clickable {
+                when (touchCounter.value) {
+                    0 -> {
+                        Toast.makeText(context, "Click again to log out", Toast.LENGTH_SHORT)
+                            .show()
+                        touchCounter.value++
+                    }
+                    1 -> {
+                        auth.signOut()
+                        context.startActivity(Intent(context, MainActivity::class.java))
+                    }
+                }
+            }
         )
         // Text to Display the current Screen
-        Text(text = "Profile")
         Text(text = "You are logged in as: ${cUser?.email}")
-        Button(onClick = {
-            auth.signOut()
-            context.startActivity(Intent(context, MainActivity::class.java))
-        }) { Text(text = "Sign out") }
     }
 }
