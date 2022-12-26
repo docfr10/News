177de70b86b3077c5a342982d5a6ef6aa66d2f79
docfr10 <docfr10@yandex.ru>
2022-12-26 10:37:16 +0300

    Updated comments

--- app/src/main/java/com/example/newsapplication/model/notifications/NotificationsModel.kt
+++ app/src/main/java/com/example/newsapplication/model/notifications/NotificationsModel.kt
@@ -12,11 +12,14 @@ import com.example.newsapplication.*
 // Class responsible for creating notifications
 class NotificationsModel : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        // Creating a login Intent in the app
         val intent1 = Intent(context, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
+        // Creating a PendingIntent to login to the app
         val pendingIntent =
             PendingIntent.getActivity(context, 0, intent1, PendingIntent.FLAG_IMMUTABLE)
+        // Creating a Notification
         val notification = NotificationCompat.Builder(context, R.string.channelID.toString())
             .setSmallIcon(R.mipmap.ic_launcher_round)
             .setContentTitle(intent.getStringExtra(R.string.titleExtra.toString()))
@@ -25,9 +28,11 @@ class NotificationsModel : BroadcastReceiver() {
             .setAutoCancel(false)
             .build()
 
+        // Sending a notification to a channel
         val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         manager.notify(notificationID, notification)
 
+        // Change the notificationID for the new notification
         notificationID++
     }
 }
